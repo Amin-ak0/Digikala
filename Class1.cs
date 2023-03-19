41eee7125c6324e21b8ac195d2cd29b3bda9ecdc
Amin-ak0 <127153434+Amin-ak0@users.noreply.github.com>
2023-03-19 21:06:02 +0330

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -3,7 +3,8 @@
 namespace lockkeyboardandcalculatenum
 {
     public class LockKeyboard
-    {
+    { 
+        string s;
         ConsoleKeyInfo digit;
         public string locknum()
         {
@@ -15,24 +16,25 @@ namespace lockkeyboardandcalculatenum
 
             return digit.KeyChar.ToString();
         }
-        public int Calculatenum(int number)
-        {
-            if (digit.Key.ToString() != "Enter")
-            {
-                number = number * 10 + Convert.ToInt32(digit.KeyChar.ToString());
-            }
-            return number;
-        }
-        //public string  continuewithlock(string s)
+        //public int Calculatenum(int number)
         //{
-        //   
-        //    Console.WriteLine("Do you want to continue ???(y/n)");
-        //    do
+        //    if (digit.Key.ToString() != "Enter")
         //    {
-        //        s = Console.ReadKey(true).KeyChar.ToString();
-
-        //    } while (s.ToLower() != "y" && s.ToLower() != "n");
-        //    return s;
+        //        number = number * 10 + Convert.ToInt32(digit.KeyChar.ToString());
+        //    }
+        //    return number;
         //}
+        public string continuewithlock()
+        {
+           
+
+            Console.WriteLine("Do you want to continue ???(y/n)");
+            do
+            {
+                s = Console.ReadKey(true).KeyChar.ToString();
+
+            } while (s.ToLower() != "y" && s.ToLower() != "n");
+            return s;
+        }
     }
 }
